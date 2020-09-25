880cb1f62f99b732ddaf950badac0730bdd12041
ramon arents <ramonarents@hotmail.com>
2020-09-25 16:48:11 +0200

    finished styling register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,26 +12,26 @@
 
                     <div class="inputBox">
                         <span class="fa fa-user"></span><x-jet-label value="{{ __('Name') }}" />
-                        <x-jet-input class="block mt-1 w-full textBox" type="text" name="name" :value="old('name')" required autofocus autocomplete="off" />
+                        <x-jet-input class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="off" />
                     </div>
 
                     <div class="mt-4 inputBox">
                         <span class="fa fa-envelope-square"></span><x-jet-label value="{{ __('Email') }}" />
-                        <x-jet-input class="block mt-1 w-full textBox" type="email" name="email" :value="old('email')" required autocomplete="off" />
+                        <x-jet-input class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="off" />
                     </div>
 
                     <div class="mt-4 inputBox">
                         <span class="fa fa-lock"></span><x-jet-label value="{{ __('Password') }}" />
-                        <x-jet-input class="block mt-1 w-full textBox" type="password" name="password" required autocomplete="off" />
+                        <x-jet-input class="block mt-1 w-full" type="password" name="password" required autocomplete="off" />
                     </div>
 
                     <div class="mt-4 inputBox">
                         <span class="fa fa-lock"></span><x-jet-label value="{{ __('Confirm Password') }}" />
-                        <x-jet-input class="block mt-1 w-full textBox" type="password" name="password_confirmation" required autocomplete="off" />
+                        <x-jet-input class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="off" />
                     </div>
 
                     <div class="flex items-center justify-end mt-4">
-                        <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+                        <a class="underline text-sm text-white-600 hover:text-white-900" href="{{ route('login') }}">
                             {{ __('Already registered?') }}
                         </a>
 
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,6 +16,9 @@
         <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css')}}">
 
         <!-- Scripts -->
+        <!-- <script src="https://code.jquery.com/jquery-3.5.1.min.js"
+                integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+                crossorigin="anonymous"></script> -->
         <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.2.1/dist/alpine.js" defer></script>
     </head>
     <body>

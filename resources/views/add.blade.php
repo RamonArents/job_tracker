d5c8c219d5d078fd86f2084554f0c12a40ecee44
ramon arents <ramonarents@hotmail.com>
2020-10-13 17:45:31 +0200

    improved responsiveness of forms

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -15,27 +15,27 @@
                     @csrf
 
                     <div class="inputBox">
-                        <x-jet-label value="{{ __('Naam') }}" />
+                        <x-jet-label value="{{ __('Naam:') }}" />
                         <x-jet-input class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="off" />
                     </div>
 
                     <div class="mt-4 inputBox">
-                        <x-jet-label value="{{ __('Beschrijving') }}" />
+                        <x-jet-label value="{{ __('Beschrijving:') }}" />
                         <textarea class="block mt-1 w-full" type="text" name="description" :value="old('description')" rows="2" cols="1" required autocomplete="off"></textarea>
                     </div>
 
                     <div class="mt-4 inputBox">
-                        <x-jet-label value="{{ __('Website') }}" />
+                        <x-jet-label value="{{ __('Website:') }}" />
                         <x-jet-input class="block mt-1 w-full" type="text" name="website" :value="old('website')" required autocomplete="off" />
                     </div>
 
                     <div class="mt-4 inputBox">
-                        <x-jet-label value="{{ __('Locatie') }}" />
+                        <x-jet-label value="{{ __('Locatie:') }}" />
                         <x-jet-input class="block mt-1 w-full" type="text" name="location" :value="old('location')" required autocomplete="off" />
                     </div>
 
                     <div class="mt-4 inputBox">
-                        <x-jet-label value="{{ __('Aangenomen') }}" />
+                        <x-jet-label value="{{ __('Aangenomen:') }}" />
                         <select name="success">
                             <option value="">Sollicitatie nog sturen.</option>
                             <option value="1">Ja</option>
--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -14,27 +14,27 @@
                         @csrf
 
                         <div class="inputBox">
-                            <x-jet-label value="{{ __('Naam') }}" />
+                            <x-jet-label value="{{ __('Naam:') }}" />
                             <x-jet-input class="block mt-1 w-full" type="text" name="name" value="{{ $job->name }}" required autofocus autocomplete="off" />
                         </div>
 
                         <div class="mt-4 inputBox">
-                            <x-jet-label value="{{ __('Beschrijving') }}" />
+                            <x-jet-label value="{{ __('Beschrijving:') }}" />
                             <textarea class="block mt-1 w-full" type="text" name="description" rows="2" cols="1" required autocomplete="off">{{ $job->job_description }}</textarea>
                         </div>
 
                         <div class="mt-4 inputBox">
-                            <x-jet-label value="{{ __('Website') }}" />
+                            <x-jet-label value="{{ __('Website:') }}" />
                             <x-jet-input class="block mt-1 w-full" type="text" name="website" value="{{ $job->website }}" required autocomplete="off" />
                         </div>
 
                         <div class="mt-4 inputBox">
-                            <x-jet-label value="{{ __('Locatie') }}" />
+                            <x-jet-label value="{{ __('Locatie:') }}" />
                             <x-jet-input class="block mt-1 w-full" type="text" name="location" value="{{ $job->location }}" required autocomplete="off" />
                         </div>
 
                     <div class="mt-4 inputBox">
-                        <x-jet-label value="{{ __('Aangenomen') }}" />
+                        <x-jet-label value="{{ __('Aangenomen:') }}" />
                         <select name="success">
                             <option value="" {{ $job->success === null ? 'selected' : ''}}>Sollicitatie nog sturen.</option>
                             <option value="1" {{ $job->success === 1 ? 'selected' : ''}}>Ja</option>

94f0f68b695e8b8d085a87c2de7e5f53b1f98963
ramon arents <ramonarents@hotmail.com>
2020-10-21 11:50:41 +0200

    no data fixen when no records

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,25 +6,30 @@
     </x-slot>
 
     <div class="dashboard-container">
-        @foreach($jobs as $job)
-        <div class="card">
-            <div class="header">
-                <a href="{{ route('getDelete', ['id' => $job->id]) }}"><span class="fa fa-trash"></span></a>
-                <a href="{{ route('getEdit', ['id' => $job->id]) }}"><span class="fa fa-edit"></span></a>
-                <form action="{{ route('addFavorite', ['id' => $job->id]) }}">
-                    @csrf
-                    <input type="hidden" name="favorite" value="{{ $job->favorite == 1 ? 0 : 1 }}">
-                    <button class="favorite" type="submit"><span class="fa fa-heart-o {{ $job->favorite == 0 || $job->favorite == null ? 'heart-black' : 'heart-red'  }}"></span></button>
-                </form>
-            </div>
-            <div class="content">
-                <h2>{{ $job->name }}</h2>
-                <p>{{ $job->job_description }}</p>
-                <a href="{{ $job->website }}" target="_blank">Website</a>
-                <p class="normal-font">Locatie: {{ $job->location }}</p>
-                <p class="normal-font">Aangenomen: {{ $job->success }}</p>
-            </div>
-        </div>
-        @endforeach
+        @if(empty($jobs))
+            <p class="no-jobs-jet">You have no jobs yet. Click <a href="{{ route('getAdd') }}">here</a> to add.</p>
+        @else
+            @foreach($jobs as $job)
+                <div class="card">
+                    <div class="header">
+                        <a href="{{ route('getDelete', ['id' => $job->id]) }}"><span class="fa fa-trash"></span></a>
+                        <a href="{{ route('getEdit', ['id' => $job->id]) }}"><span class="fa fa-edit"></span></a>
+                        <form action="{{ route('addFavorite', ['id' => $job->id]) }}">
+                            @csrf
+                            <input type="hidden" name="favorite" value="{{ $job->favorite == 1 ? 0 : 1 }}">
+                            <button class="favorite" type="submit"><span class="fa fa-heart-o {{ $job->favorite == 0 || $job->favorite == null ? 'heart-black' : 'heart-red'  }}"></span></button>
+                        </form>
+                    </div>
+                    <div class="content">
+                        <h2>{{ $job->name }}</h2>
+                        <p>{{ $job->job_description }}</p>
+                        <a href="{{ $job->website }}" target="_blank">Website</a>
+                        <p class="normal-font">Locatie: {{ $job->location }}</p>
+                        <p class="normal-font">Aangenomen: {{ $job->success }}</p>
+                    </div>
+                </div>
+                @endforeach
+        @endif
         <a href="{{ route('getAdd') }}" id="topBtn"><span class="fa fa-plus"></span></a>
+    </div>
 </x-app-layout>
